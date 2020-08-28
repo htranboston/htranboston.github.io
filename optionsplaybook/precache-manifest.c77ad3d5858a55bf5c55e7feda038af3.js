dc436a037613a537970b864d2541a5bf14741641
Huy Tran <huy.m.tran@gmail.com>
2020-08-27 23:12:47 -0400

    option playbook app

---
+++ optionsplaybook/precache-manifest.c77ad3d5858a55bf5c55e7feda038af3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "671748351341ad1d0aee4caedec9a018",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c54e8cc2c53b6042f50",
+    "url": "/static/css/main.667005be.chunk.css"
+  },
+  {
+    "revision": "9736ebf87e0a9ef0fc47",
+    "url": "/static/js/2.c225f5dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c225f5dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c54e8cc2c53b6042f50",
+    "url": "/static/js/main.9f88fd65.chunk.js"
+  },
+  {
+    "revision": "e4fe675201fad9750ec3",
+    "url": "/static/js/runtime-main.08528d3c.js"
+  }
+]);
